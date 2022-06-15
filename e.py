88191af2650256bee5ff520aa457cbeb44dd194c
vaibhavpatil872 <vaibhavpatil872@gmail.com>
2022-06-15 13:03:48 +0530

    changes made in e file fibo_gen

--- e.py
+++ e.py
@@ -1,7 +1,15 @@
 print("hello")
-n=5
-for i in range(n):
-    print(' '*(n-i-1)+("*"+' ')*(i+1))
-for j in range(n):
-    print(' '*(j+1)+("*"+' ')*(n-j-1))
+def fibo_gen():
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b,a+b
+f1=fibo_gen()
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
+print(next(f1))
     
